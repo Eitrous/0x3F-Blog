8f2c2b37bb17a782127478165d3f85dec61d9b2b
Eitrous <hzx.zxhuang@outlook.com>
2026-01-23 03:51:30 +0800

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 yum install wget
 mkdir pandoc
-wget -qO- https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-amd64.tar.gz | \
+wget -qO- https://github.com/jgm/pandoc/releases/download/3.8.3/pandoc-3.8.3-linux-amd64.tar.gz | \
    tar xvzf - --strip-components 1 -C ./pandoc
 export PATH="./pandoc/bin:$PATH"
 
