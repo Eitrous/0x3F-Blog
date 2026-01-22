236bc75c539ed52eebbd461f36ba534f6e7cf6a8
Eitrous <hzx.zxhuang@outlook.com>
2026-01-23 04:16:53 +0800

    modified:   .gitignore 	deleted:    build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-yum install wget
-mkdir pandoc
-wget -qO- https://github.com/jgm/pandoc/releases/download/3.8.3/pandoc-3.8.3-linux-amd64.tar.gz | \
-   tar xvzf - --strip-components 1 -C ./pandoc
-export PATH="./pandoc/bin:$PATH"
-
-# yarn run build
-npm run build
